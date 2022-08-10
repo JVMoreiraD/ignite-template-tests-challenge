cd8277d933420da072b999de671df54ae25f739f
jvmoreirad <jvitormoreirad@hotmail.com>
2022-08-10 17:48:44 -0300

    feat: test criar user com email repetido

--- src/modules/users/useCases/createUser/CreateUserUseCase.spec.ts
+++ src/modules/users/useCases/createUser/CreateUserUseCase.spec.ts
@@ -1,4 +1,5 @@
 import { InMemoryUsersRepository } from "../../repositories/in-memory/InMemoryUsersRepository";
+import { CreateUserError } from "./CreateUserError";
 import { CreateUserUseCase } from "./CreateUserUseCase"
 
 let createUserUseCase: CreateUserUseCase;
@@ -21,4 +22,20 @@ describe("Tests for CreateUserUseCase", () => {
     expect(user).toHaveProperty("id")
 
   });
+
+  it("Should not be able to create a new user with a email that is taken", async () => {
+    expect(async () => {
+      await createUserUseCase.execute({
+        name: "João",
+        email: "joao@test.com",
+        password: "123"
+      });
+      await createUserUseCase.execute({
+        name: "João Segundo",
+        email: "joao@test.com",
+        password: "123"
+      })
+    }).rejects.toBeInstanceOf(CreateUserError)
+
+  })
 })
